bb25d5a83d41330598d5537caeed4a77ca318469
Jaentigro <36735017+joovee@users.noreply.github.com>
2019-02-08 09:54:16 +0800

    first commit

---
+++ profile.php
@@ -0,0 +1,64 @@
+<?php
+require('db.php');
+include("authentication.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profile</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+	<link rel="stylesheet" href="style.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body background="photos/Personal CollectionNewlogo_20101006172454_10.jpeg" alt="Responsive background-image" >
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <a class="navbar-brand" href="home.html">
+			<img src="photos/pc.png" width="50" height="50" alt=""></a>
+ 	    </a>
+           
+            <li class="nav-item ">
+              <a class="nav-link " href="everydayincome.html" >
+               Home 
+              </a>
+            </li>
+			<li class="nav-item ">
+              <a class="nav-link " href="budgetlist.html" >
+               About us
+              </a>
+            </li>
+			<li class="nav-item ">
+              <a class="nav-link " href="editlist.html" >
+               Product
+              </a>
+            </li>
+			<li class="nav-item ">
+              <a class="nav-link " href="about.html" >
+               Cart
+              </a>
+            </li>
+			<li class="nav-item ">
+              <a class="nav-link " href="logout.php" >
+               Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+		</button>
+    </nav><br/><br/>
+
+<header>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><div>
+<center>
+<h1>Welcome Vee</h1><br/><br/>
+</center>
+</div>
+</div>
+</body>
+</html>
